683c12709026dfb4ec5347ab83e64aa353e71c20
Andrew Littlejohn <handwrittenjello@gmail.com>
2019-07-30 12:23:40 -0400

    Added Database and created fightcard table

--- Wiki Scraper.py
+++ Wiki Scraper.py
@@ -43,3 +43,71 @@ df['Time'] = victoryTime
 df['Notes'] = notes
 df = df.replace('\n','', regex=True)
 print (df)
+
+import sqlite3
+from sqlite3 import Error
+
+"""-- fightcard table
+CREATE TABLE IF NOT EXISTS fightcard table (
+ weightClass text,  
+ fighterWinner text PRIMARY KEY,
+ defeat text,
+ fighterLoser text,
+ victoryType text,
+ victoryRound text,
+ victoryTime text,
+ notes text,
+
+);"""
+
+def create_connection(db_file):
+    # create a database connection to the SQLite database
+    #    specified by db_file
+    #:param db_file: database file
+    #:return: Connection object or None
+    
+    try:
+        conn = sqlite3.connect('PythonDatabase.db')
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None
+
+def create_table(conn, create_table_sql):
+    # create a table from the create_table_sql statement
+    #:param conn: Connection object
+    #:param create_table_sql: a CREATE TABLE statement
+    #:return:
+    
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def main():
+    database = "/Users/andrewlittlejohn/projects/AutomatedEdit/Python Database.db"
+
+    sql_create_fightcard_table =  """CREATE TABLE IF NOT EXISTS fightcard (
+                                        weightClass text NOT NULL,  
+                                        fighterWinner text,
+                                        defeat text NOT NULL,
+                                        fighterLoser text NOT NULL,
+                                        victoryType text NOT NULL,
+                                        victoryRound text NOT NULL,
+                                        victoryTime text NOT NULL,
+                                        notes text NOT NULL
+                                    ); """
+ 
+ 
+    # create a database connection
+    conn = create_connection('PythonDatabase.db')
+    if conn is not None:
+        # create UFC table
+        create_table(conn, sql_create_fightcard_table)
+    else:
+        print("Error! cannot create the database connection.")
+
+if __name__ == '__main__':
+    main()
