a52fc86d377397d946723fe34a9ab5dc91544fcf
Andrew Littlejohn <handwrittenjello@gmail.com>
2019-08-03 20:44:21 -0400

    Updated Splitting file to allow user input

--- MKVSplit.py
+++ MKVSplit.py
@@ -1,5 +1,14 @@
 
 import subprocess
-startTime = '2s'
-endTime = '6s'
-subprocess.call([r'mkvmerge','-o','test1.mkv','test.mkv', '--split', 'timestamps:'+startTime+','+endTime])
+print('Please enter the name of the file you would like to edit:')
+filenameInput = input()
+print('Please enter the start of the First Fight')
+firstFightStartInput = input()
+firstFightStart = firstFightStartInput[:2] + ':' + firstFightStartInput[2:4] + ':' + firstFightStartInput[4:6]
+print(firstFightStart)
+print('Please enter the end of the First Fight')
+firstFightEndInput = input()
+firstFightEnd = firstFightEndInput[:2] + ':' + firstFightEndInput[2:4] + ':' + firstFightEndInput[4:6]
+print(firstFightEnd)
+#endTime = str(input())
+subprocess.call([r'mkvmerge','-o', filenameInput + 'split.mkv', filenameInput + '.mkv', '--split', 'timestamps:'+firstFightStart+','+firstFightEnd])
