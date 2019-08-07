d10cfa37ebea34e8b357d8973292298881003b71
Andrew Littlejohn <handwrittenjello@gmail.com>
2019-08-07 12:28:15 -0400

    Added Batch Transcoding Automation to mix down

---
+++ batchEncodeMac.py
@@ -0,0 +1,29 @@
+import os
+import time
+import subprocess
+import sys
+
+fileList = []
+rootdir = "/Users/andrewlittlejohn/projects/AutomatedEdit/flask"
+for root, subFolders, files in os.walk(rootdir):
+    for file in files:
+        theFile = os.path.join(root,file)
+        fileName, fileExtension = os.path.splitext(theFile)
+        if fileExtension.lower() in ('.avi', '.divx', '.flv', '.m4v', '.mkv', '.mov', '.mpg', '.mpeg', '.wmv'):
+            print('Adding',theFile)
+            fileList.append(theFile)
+
+runstr = '/Users/andrewlittlejohn/bin/HandBrakeCLI -i "{0}" -o "{1}" --preset-import-file drew.json'
+
+print('=======--------=======')
+
+while fileList:
+    inFile = fileList.pop()
+    fileName, fileExtension = os.path.splitext(inFile)
+    outFile = fileName+'1'+'.mkv'
+
+    print('Processing',inFile)
+    returncode  = subprocess.call(runstr.format(inFile,outFile),shell=True)
+    time.sleep(5)
+    print('Removing',inFile)
+    os.remove(inFile)
---
+++ batchEncodeWindows.py
@@ -0,0 +1,29 @@
+import os
+import time
+import subprocess
+import sys
+
+fileList = []
+rootdir = #Enter Root directory as String here "Root Directory :"
+for root, subFolders, files in os.walk(rootdir):
+    for file in files:
+        theFile = os.path.join(root,file)
+        fileName, fileExtension = os.path.splitext(theFile)
+        if fileExtension.lower() in ('.avi', '.divx', '.flv', '.m4v', '.mkv', '.mov', '.mpg', '.mpeg', '.wmv'):
+            print('Adding',theFile)
+            fileList.append(theFile)
+#Enter Handbrake CLI Location here Handbrake preset should be in same directory
+runstr = 'HANDBRAKE CLI LOCATION HERE -i "{0}" -o "{1}" --preset-import-file drew.json'
+
+print('=======--------=======')
+
+while fileList:
+    inFile = fileList.pop()
+    fileName, fileExtension = os.path.splitext(inFile)
+    outFile = fileName+'1'+'.mkv'
+
+    print('Processing',inFile)
+    returncode  = subprocess.call(runstr.format(inFile,outFile),shell=True)
+    time.sleep(5)
+    print('Removing',inFile)
+    os.remove(inFile)
