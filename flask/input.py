531fcd5cf6065328a3a15a6f3f7ef3db32e272f4
Andrew Littlejohn <handwrittenjello@gmail.com>
2019-08-12 13:53:10 -0400

    Added Input UI

--- flask/input.py
+++ flask/input.py
@@ -1,6 +1,6 @@
 import subprocess
 import webbrowser
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, flash,redirect
 import requests
 import numpy as np
 import os
@@ -9,15 +9,29 @@ from tmdbv3api import Movie
 import urllib
 from bs4 import BeautifulSoup
 import pandas as pd
+from functiontest import function
+from form import RegistrationForm, LoginForm, inputForm
 
 #Run Flask
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
 
 
+@app.route('/', methods=['GET', 'POST'])
+def register():
+    form = inputForm()
+    if form.validate_on_submit():
+        flash(f'Data Sent for {form.cardNumber.data}!', 'success')
+        return redirect(url_for('login'))
 
-@app.route('/')
-def index():
-    return render_template('input.html')
+    return render_template('input.html', form=form)
+
+@app.route('/split', methods=["GET", "POST"])
+def login():
+    form = RegistrationForm()
+    return render_template('register.html', form=form, inputForm=inputForm)
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
