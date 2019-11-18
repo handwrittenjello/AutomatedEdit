6f3ec41f09bc4f1601f331b9a63b02c73291f824
Andrew Littlejohn <handwrittenjello@gmail.com>
2019-11-17 19:03:04 -0500

    Updated Batch Encode Formatting

--- batchEncodeMac.py
+++ batchEncodeMac.py
@@ -7,10 +7,10 @@ fileList = []
 rootdir = "/Users/andrewlittlejohn/projects/AutomatedEdit/flask"
 for root, subFolders, files in os.walk(rootdir):
     for file in files:
-        theFile = os.path.join(root,file)
+        theFile = os.path.join(root, file)
         fileName, fileExtension = os.path.splitext(theFile)
         if fileExtension.lower() in ('.avi', '.divx', '.flv', '.m4v', '.mkv', '.mov', '.mpg', '.mpeg', '.wmv'):
-            print('Adding',theFile)
+            print('Adding', theFile)
             fileList.append(theFile)
 
 runstr = '/Users/andrewlittlejohn/bin/HandBrakeCLI -i "{0}" -o "{1}" --preset-import-file drew.json'
@@ -22,8 +22,8 @@ while fileList:
     fileName, fileExtension = os.path.splitext(inFile)
     outFile = fileName+'1'+'.mkv'
 
-    print('Processing',inFile)
-    returncode  = subprocess.call(runstr.format(inFile,outFile),shell=True)
+    print('Processing', inFile)
+    returncode = subprocess.call(runstr.format(inFile, outFile), shell=True)
     time.sleep(5)
-    print('Removing',inFile)
+    print('Removing', inFile)
     os.remove(inFile)
